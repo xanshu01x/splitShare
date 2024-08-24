629aed2cad9add2f1ad956d2b07d7cac8107e44f
Priyanshu <anshusuper111@gmail.com>
2024-08-24 15:44:10 +0530

    API

--- api/app.js
+++ api/app.js
@@ -6,7 +6,9 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-console.log('Heya!');
+app.use('/api/data', (req, res) => {
+    res.send('Hello Worldssss!');
+});
 app.listen(8800, () => {
     console.log('Server is running on port 8800');
 });
