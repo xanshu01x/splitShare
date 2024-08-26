00798af782595b9b64b4e7df4a6b605ca492bbd5
Priyanshu <anshusuper111@gmail.com>
2024-08-26 20:55:05 +0530

    Protected and UnProtected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,57 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import {
+    createBrowserRouter,
+    Navigate,
+    RouterProvider
+} from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Profile from './pages/Profile';
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
+import PropTypes from 'prop-types';
 
+// A wrapper component to handle protected routes
+const PrivateRoute = ({ element }) => {
+    const { currentUser } = useContext(AuthContext);
+
+    return currentUser ? element : <Navigate to='/login' />;
+};
+
+const PublicRoute = ({ element }) => {
+    const { currentUser } = useContext(AuthContext);
+    return currentUser ? <Navigate to='/' /> : element;
+};
+
+// Define routes using createBrowserRouter
+const router = createBrowserRouter([
+    {
+        path: '/login',
+        element: <PublicRoute element={<Login />} />
+    },
+    {
+        path: '/signup',
+        element: <PublicRoute element={<Signup />} />
+    },
+    {
+        path: '/',
+        element: <PrivateRoute element={<Dashboard />} />
+    },
+    {
+        path: '/profile',
+        element: <PrivateRoute element={<Profile />} />
+    }
+]);
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path='/' element={<Dashboard />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/signup' element={<Signup />} />
-                <Route path='/profile' element={<Profile />} />
-            </Routes>
-        </>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
+
+PrivateRoute.propTypes = {
+    element: PropTypes.element.isRequired
+};
+PublicRoute.propTypes = {
+    element: PropTypes.element.isRequired
+};
